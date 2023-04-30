114a05c11ff78844c1d5b89247b988a40bddd805
AlexanderMilton47159375 <Alexander.Milton1@students.mq.edu.au>
2023-04-30 15:22:05 +1000

    win loss and restart/exit now possible also

--- consoleMineSweeper/src/consoleMineSweeper/Main.java
+++ consoleMineSweeper/src/consoleMineSweeper/Main.java
@@ -11,23 +11,40 @@ public class Main {
 		int gameBoardSize = s.nextInt();
 		System.out.println("Please enter the number of Bombs (>=1 <=width^2)");
 		int numberOfBombs = s.nextInt();
-		boolean gameOver = false;
+
+		boolean running = true;
 		
 		Grid gameGrid = new Grid(numberOfBombs, gameBoardSize);
 		gameGrid.initialiseBombs();
 		
 		
 		
-		while(!gameOver) {
+		while(running) {
 			gameGrid.render();
 			System.out.println("Enter tile number");
 			int input = s.nextInt();
-			Cell selected = gameGrid.getCell(input);
+			Cell selected;
+			try {
+			selected = gameGrid.getCell(input);
+			}catch(Exception e) {
+				System.out.println("please provide a valid input");
+				continue;
+			}
 			boolean bomb = selected.getBomb();
 			if(bomb) {
 				System.out.println("BOOOM!");
-				gameOver = true;
-				break;
+				System.out.print("type r to retry or any key to exit");
+				s.nextLine();
+				String menuInput = s.nextLine();
+				System.out.println(menuInput);
+				if(menuInput.equals("r")) {
+					gameGrid = new Grid(numberOfBombs, gameBoardSize);
+					gameGrid.initialiseBombs();
+					continue;
+				}else {
+					break;
+				}
+				
 			}else {
 				gameGrid.decrementSafeSpacesLeft();
 				gameGrid.showBombs(selected);
@@ -35,11 +52,24 @@ public class Main {
 			
 			if(gameGrid.getSafeSpacesLeft() == 0) {
 				System.out.println("You Won!!!");
-				gameOver = true;
-				break;
-			}
+				System.out.print("type r to retry or any key to exit");
+				s.nextLine();
+				String menuInput = s.nextLine();
+				System.out.println(menuInput);
+				if(menuInput.equals("r")) {
+					gameGrid = new Grid(numberOfBombs, gameBoardSize);
+					gameGrid.initialiseBombs();
+					continue;
+				}else {
+					break;
+				}
+			
+				
+		}
+		
 			
 		}
+		System.out.println("exited");
 
 	}
 
