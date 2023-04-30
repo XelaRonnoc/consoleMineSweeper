c1e24b39d590448bba923ace5cf44fe13579975e
AlexanderMilton47159375 <Alexander.Milton1@students.mq.edu.au>
2023-04-30 14:24:10 +1000

    base game complete, no win condition yet, no modularity yet

---
+++ consoleMineSweeper/src/consoleMineSweeper/Cell.java
@@ -0,0 +1,62 @@
+package consoleMineSweeper;
+
+public class Cell {
+	private int xLoc;
+	private int yLoc;
+	private boolean rowEnd;
+	private boolean hasBomb = false;
+	private String innerName;
+	private String name;
+	private boolean revealed = false;
+	
+	
+	public Cell(int x, int y, boolean rowEnd) {
+		this.xLoc = x;
+		this.yLoc = y;
+		this.rowEnd = rowEnd;
+		this.name = "| " + this.xLoc + this.yLoc + " |";
+		this.innerName = "" + this.xLoc + this.yLoc;
+		
+	}
+	
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(int numBombs) {
+		this.name = "| " + "B" + numBombs + " |";
+	}
+	
+	public int getXLoc() {
+		return this.xLoc;
+	}
+	public int getYLoc() {
+		return this.yLoc;
+	}
+	
+	public void render() {
+		if(this.hasBomb) {
+			System.out.print("| xx |");
+		}else {
+			System.out.print(this.name);
+		}
+		
+		if(this.rowEnd) {
+			System.out.printf("\n");
+		}
+		
+	}
+	
+	public void setBomb() {
+		this.hasBomb = true;
+	}
+	
+	public boolean getBomb() {
+		return this.hasBomb;
+	}
+	
+
+	
+
+}
--- consoleMineSweeper/src/consoleMineSweeper/Main.java
+++ consoleMineSweeper/src/consoleMineSweeper/Main.java
@@ -1,9 +1,34 @@
 package consoleMineSweeper;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner s = new Scanner(System.in);
+		boolean gameOver = false;
+		
+		Grid gameGrid = new Grid(100, 10);
+		gameGrid.initialiseBombs();
+		
+		
+		
+		while(!gameOver) {
+			gameGrid.render();
+			System.out.println("Enter tile number");
+			int input = s.nextInt();
+			Cell selected = gameGrid.getCell(input);
+			boolean bomb = selected.getBomb();
+			if(bomb) {
+				System.out.println("BOOOM!");
+				gameOver = true;
+				break;
+			}else {
+				gameGrid.showBombs(selected);;
+			}
+			
+		}
 
 	}
 
